060c4b9ec01ea7d1269b841168999bcc41b6f34a
edgar96roa <edgar.roa.c.1996@gmail.com>
2022-01-04 18:38:31 -0600

    Control Props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -8,38 +8,21 @@ export const useShoppingCart = () => {
 
         setShoppingCart( oldShoppingCart => {
 
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
+            if( count===0 ){
+                
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+                
+                console.log({ toDelete });
 
-            if( Math.max( productInCart.count + count, 0 ) > 0 ) {
-                productInCart.count += count;
                 return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
+                    ...rest
                 }
             }
 
-            // Borrar el producto
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-
             return {
-                ...rest
+                ...oldShoppingCart,
+                [product.id] : { ...product, count }
             }
-
-            // if( count===0 ){
-                
-            //     const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-                
-            //     console.log({ toDelete });
-
-            //     return {
-            //         ...rest
-            //     }
-            // }
-
-            // return {
-            //     ...oldShoppingCart,
-            //     [product.id] : { ...product, count }
-            // }
         } );
     }
 
